24fc2a84c09ca5fc03401469a489b0f87599999e
smit4446 <smit4446@gmail.com>
2018-06-07 14:33:05 -0500

    to fight off evil devs

--- server/routes/song-router.js
+++ server/routes/song-router.js
@@ -21,22 +21,38 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log('In song-router POST to create');
-  res.sendStatus(200);
+  const queryText = `INSERT INTO songs (artist, track, published, rank)
+  VALUES ($1, $2, $3,$4);`;
+  pool.query(queryText, [req.body.artist, req.body.track, req.body.published, req.body.rank])
+  .then((results) => {
+    res.sendStatus(200);
+  }).catch((err) => {
+    console.log('Error getting all songs: ', err);
+    res.sendStatus(500);
+  })
 });
 
-router.put('/', (req, res) => {
+router.put('/:id', (req, res) => {
   console.log('In song-router PUT to update');
+  const queryText = `UPDATE songs SET track=$1 WHERE artist=$2;`;
+  pool.query(queryText, [req.body.track, req.params.id])
+  .then((results) => {
+    res.sendStatus(200);
+  }).catch((err) => {
+    console.log('Error updating song: ', err);
+    res.sendStatus(500);
+  })
   res.sendStatus(200);
 });
 
 router.delete('/:id', (req, res) => {
-  const taco = req.params.id;
+  const id = req.params.id;
   console.log('In song-router DELETE to delete');
-  const queryText = 'DELETE FROM songs WHERE artist=$2 id=$1';
+  const queryText = 'DELETE FROM songs WHERE id=$1';
   // Passing two things to the query. 1) the query text
   // 2) the values to substitute into the query for the $1, $2, etc. 
   //    when subbing in multiple things, the order is important.
-  pool.query(queryText, [taco, 'someone'])
+  pool.query(queryText, [id])
     .then((results)=>{
       console.log('Successful delete of song', results);
       res.sendStatus(200);
